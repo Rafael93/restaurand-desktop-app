92623fbf1850f2650ae5a6a44ca1ae2a705a421e
Rafael93 <rafaelgamas@gmail.com>
2024-09-10 15:03:21 -0400

    finishing base structure

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
+console.log("Running electron client");
 const { app, BrowserWindow } = require("electron");
 const path = require("node:path");
-const createWindow = () => {
+const { stdout } = require("process");
+
+function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1600,
+    height: 1000,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
   });
   win.loadFile("index.html");
-};
+}
+
 app.whenReady().then(() => {
   createWindow();
-  app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") app.quit();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
   });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
